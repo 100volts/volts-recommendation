80989ece5ef1e05a2f20c53895dadba2ad1854e4
IvoRum <ivailo.rumenov@outlook.com>
2024-10-18 07:26:26 +0300

    first commit

---
+++ volts-recommendation/src/main/java/com/volts/recommendation/VoltsRecommendationApplication.java
@@ -0,0 +1,13 @@
+package com.volts.recommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoltsRecommendationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoltsRecommendationApplication.class, args);
+    }
+
+}
